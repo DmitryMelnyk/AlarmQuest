a97e4cf36392040689b1e1063a1a4d4bf867ea53
DmitryMelnyk <dmitrymelnyk@yahoo.com>
2017-12-19 02:32:02 +0200

    Removed build files

--- app/src/main/java/com/dmelnyk/alarmquest/utils/AlarmClockUtil.java
+++ app/src/main/java/com/dmelnyk/alarmquest/utils/AlarmClockUtil.java
@@ -1,8 +1,52 @@
 package com.dmelnyk.alarmquest.utils;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import java.util.Calendar;
+
 /**
  * Created by d264 on 6/14/17.
  */
 
 public class AlarmClockUtil {
+    private Context mContext;
+    private AlarmManager alarmManager;
+    private PendingIntent alarmIntent;
+
+    public AlarmClockUtil(Context mContext) {
+        this.mContext = mContext;
+        alarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(mContext, AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(mContext, 0, intent, 0);
+    }
+
+
+    /**
+     * Creates pending alarm using given time
+     *
+     * @param time The alarm's time in format 'hours:minutes'
+     */
+    public void startAlarmClock(String time) {
+        String[] hourMinute = time.split(":"); // hourMinute[0]
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hourMinute[0]));
+        calendar.set(Calendar.MINUTE, Integer.parseInt(hourMinute[1]));
+
+//        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+//                AlarmManager.INTERVAL_DAY, alarmIntent);
+    }
+
+    /**
+     * Cancels pending alarm intent
+     */
+    public void stopAlarmClock() {
+        if (alarmManager != null) {
+            alarmManager.cancel(alarmIntent);
+        }
+    }
 }
--- app/src/main/java/com/dmelnyk/alarmquest/utils/AlarmReceiver.java
+++ app/src/main/java/com/dmelnyk/alarmquest/utils/AlarmReceiver.java
@@ -1,8 +1,41 @@
 package com.dmelnyk.alarmquest.utils;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import timber.log.Timber;
+
 /**
  * Created by d264 on 6/14/17.
  */
 
-class AlarmReceiver {
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Timber.d("receiver starts!");
+
+        // TODO: create new
+//        PendingIntent pendingIntent = PendingIntent.getActivity(
+//                context, 0, new Intent(context, AlarmQuestActivity.class), 0);
+//
+//        NotificationCompat.Builder mBuilder =
+//                new NotificationCompat.Builder(context)
+//                        .setSmallIcon(R.drawable.ic_alarm_notification)
+//                        .setContentTitle("My notification")
+//                        .setContentText("Hello World!")
+//                        .setAutoCancel(true)
+//                        .setContentIntent(pendingIntent);
+//
+//        int mId = 100001;
+//        ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE))
+//                .notify(mId, mBuilder.build());
+
+        // TODO: start alarm mysic
+//        AudioService.startPlaying(context);
+        Intent startIntent = new Intent(context, AudioService.class);
+        startIntent.setAction(AudioService.PLAY);
+        context.startService(startIntent);
+    }
 }
